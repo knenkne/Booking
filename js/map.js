fe81897dfa6eab9bcf9ec889800bdf17491e21a2
AlexanderBrez <jztenk@gmail.com>
2018-11-05 01:51:02 +0300

    some card

--- js/map.js
+++ js/map.js
@@ -1,8 +1,6 @@
 'use strict';
 
-document.querySelector('.map').classList.remove('map--faded');
-
-var HOUSE_NAME = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде']
+var HOUSE_NAME = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var HOUSE_PRICE = {
   min: 1000,
   max: 1000000
@@ -11,12 +9,13 @@ var HOUSE_LOCATION = {
   x: 600,
   y: 350
 };
-var HOUSE_TYPE = {
-  palace: 'Дворец',
-  flat: 'Квартира',
-  house: 'Дом',
-  bungalo: 'Бунгало'
-};
+var PIN_LOCATION = {
+  minX: 130,
+  maxX: 630,
+  minY: 130,
+  maxY: 630
+}
+var HOUSE_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var HOUSE__ROOMS = {
   min: 1,
   max: 5
@@ -31,6 +30,14 @@ var getRandomNumber = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+var getRandomNumberFromTwo = function (min, max) {
+  return Math.floor(Math.random() * max) + min;
+};
+
+var getRandomLength = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
+
 var card = [
   {
     author: {
@@ -38,21 +45,23 @@ var card = [
     },
 
     offer: {
-      title: HOUSE_NAME[4],
-      address: HOUSE_LOCATION.x,
-      price: HOUSE_PRICE.min,
+      title: getRandomNumber(HOUSE_NAME),
+      address: '500, 500',
+      price: getRandomNumberFromTwo(HOUSE_PRICE.min, HOUSE_PRICE.max),
       type: getRandomNumber(HOUSE_TYPE),
-      rooms: HOUSE__ROOMS.max,
+      rooms: getRandomNumberFromTwo(HOUSE__ROOMS.min, HOUSE__ROOMS.max),
       guests: '5',
       checkin: getRandomNumber(CHECKIN),
       checkout: getRandomNumber(CHECKOUT),
-      features: FEATURES[5],
+      features: getRandomLength(FEATURES),
       description: '',
       photos: PHOTOS
     },
 
     location: {
-      x: HOUSE_LOCATION.x,
-      y: HOUSE_LOCATION.y
+      x: getRandomNumberFromTwo(PIN_LOCATION.minX, PIN_LOCATION.maxX),
+      y: getRandomNumberFromTwo(PIN_LOCATION.minY, PIN_LOCATION.maxY)
     }
   }];
+
+document.querySelector('.map').classList.remove('map--faded');
