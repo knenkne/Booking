bce7c40a5423b63674f38d3bcb481df7372ac162
AlexanderBrez <jztenk@gmail.com>
2018-11-21 17:12:19 +0300

    Pins

--- js/map.js
+++ js/map.js
@@ -38,7 +38,7 @@ var USER_AVATAR = {
   path: 'img/avatars/user',
   type: 'png'
 };
-var ADS_MAX = 8;
+var MAX_ADS = 8;
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -48,35 +48,69 @@ var getRandomArrayElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var ad = {
-  author: {
-    avatar: ''
-  },
-  offer: {
-    title: getRandomArrayElement(HOUSE_TITLE),
-    address: '',
-    price: getRandomNumber(HOUSE_PRICE.min, HOUSE_PRICE.max),
-    type: '',
-    rooms: getRandomNumber(HOUSE_ROOMS.min, HOUSE_ROOMS.max),
-    guests: getRandomNumber(HOUSE_GUESTS.min, HOUSE_GUESTS.max),
-    checkin: getRandomArrayElement(HOUSE_CHECKIN),
-    checkout: getRandomArrayElement(HOUSE_CHECKOUT),
-    features: '',
-    description: '',
-    photos: ''
-  },
-  location: {
-    x: getRandomNumber(PIN.min.x, PIN.max.x),
-    y: getRandomNumber(PIN.min.y, PIN.max.y)
-  }
+var getRandomProperty = function (obj) {
+  var keys = Object.keys(obj);
+  return obj[keys[Math.floor(keys.length * Math.random())]];
 };
 
-var renderAds = function () {
+// Генерируем аватарки
+var AVATARS = [];
+for (var i = 0; i < MAX_ADS; i++) {
+  AVATARS.push('0' + (i + 1) + '.');
+}
+
+// Генерируем несколько объявлений
+var generateAds = function () {
   var ads = [];
-  for (var i = 0; i < ADS_MAX; i++) {
+  for (var j = 0; j < MAX_ADS; j++) {
+    var ad = {
+      author: {
+        avatar: USER_AVATAR.path + AVATARS[j] + USER_AVATAR.type
+      },
+      offer: {
+        title: getRandomArrayElement(HOUSE_TITLE),
+        address: location.x + ', ' + location.y,
+        price: getRandomNumber(HOUSE_PRICE.min, HOUSE_PRICE.max),
+        type: getRandomProperty(HOUSE_TYPE),
+        rooms: getRandomNumber(HOUSE_ROOMS.min, HOUSE_ROOMS.max),
+        guests: getRandomNumber(HOUSE_GUESTS.min, HOUSE_GUESTS.max),
+        checkin: getRandomArrayElement(HOUSE_CHECKIN),
+        checkout: getRandomArrayElement(HOUSE_CHECKOUT),
+        features: '',
+        description: '',
+        photos: ''
+      },
+      location: {
+        x: getRandomNumber(PIN.min.x, PIN.max.x),
+        y: getRandomNumber(PIN.min.y, PIN.max.y)
+      }
+    };
     ads.push(ad);
   }
+  return ads;
+};
+var ADS = generateAds();
+
+// Генерируем метку
+var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var renderPin = function () {
+  var pinElement = similarPinTemplate.cloneNode(true);
+
+  pinElement.style.left = ADS[j].location.x + 'px';
+  pinElement.style.top = ADS[j].location.y + 'px';
+  pinElement.querySelector('img').alt = ADS[j].offer.title;
+  pinElement.querySelector('img').src = ADS[j].author.avatar;
+
+  return pinElement;
 };
 
+// Генерируем несколько меток и карточек
+var fragment = document.createDocumentFragment();
+var similarPinList = document.querySelector('.map__pins');
+var map = document.querySelector('.map');
+for (var j = 0; j < MAX_ADS; j++) {
+  fragment.appendChild(renderPin());
+}
+similarPinList.appendChild(fragment);
+
 document.querySelector('.map').classList.remove('map--faded');
-renderAds();
