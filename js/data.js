c9f4832d74d645ee900ca9477b68b9947c948a5c
KuznetsovMA <jztenk@gmail.com>
2020-01-11 20:19:49 +0300

    mock to data

--- js/data.js
+++ js/data.js
@@ -1,76 +1,35 @@
 'use strict';
 
 (function () {
-  var titles = ['Hello', 'Oh, Here We go Again', 'Amazing Apps'];
-  var types = ['palace', 'flat', 'house', 'bungalo'];
-  var checks = ['12:00', '13:00', '14:00'];
-  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var descriptions = ['None', 'ASAP Need $$$', 'Comfortable Apps'];
-  var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  function getData(url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.responseType = 'json';
 
-  var price = {
-    'min': 100,
-    'max': 1000
-  };
-
-  var coords = {
-    x: {
-      'min': 0,
-      'max': 750
-    },
-    y: {
-      'min': 130,
-      'max': 630
-    }
-  };
-
-
-  function getRandomItem(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-  function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (1 + min - max)) + max;
-  }
-
-  function createOffer(id) {
-    return {
-      'author': {
-        'avatar': id < 10 ? 'img/avatars/user0' + id + '.png' : 'img/avatars/user' + id + '.png'
-      },
-      'offer': {
-        'title': getRandomItem(titles),
-        'address': getRandomNumber(0, 500) + ', ' + getRandomNumber(0, 500),
-        'price': getRandomNumber(price.min, price.max),
-        'type': getRandomItem(types),
-        'rooms': getRandomNumber(0, 10),
-        'guests': getRandomNumber(0, 10),
-        'checkin': getRandomItem(checks),
-        'checkout': getRandomItem(checks),
-        'features': features,
-        'description': getRandomItem(descriptions),
-        'photos': photos
-      },
-      'location': {
-        'x': getRandomNumber(coords.x.min, coords.x.max),
-        'y': getRandomNumber(coords.y.min, coords.y.max)
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
-    };
-  }
+    });
 
-  function createOffers(amount) {
-    var offers = [];
-    for (var i = 1; i <= amount; i++) {
-      var offer = createOffer(i);
-      offers.push(offer);
-    }
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
 
-    return offers;
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', url);
+    xhr.send();
   }
 
   window.data = {
-    createOffers: createOffers
+    get: getData
   };
 
 }());
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var createOffers = window.data.createOffers;
+var getOffers = window.data.get;
 var renderPins = window.pin.render;
 
-var MAX_OFFERS = 8;
+var MAX_OFFERS = 5;
+var DATA_URL = 'https://js.dump.academy/keksobooking/data';
 
-var map = document.querySelector('.map');
+var mainContainer = document.querySelector('main');
+var map = mainContainer.querySelector('.map');
 var mainPin = map.querySelector('.map__pin--main');
 var pinsContainer = map.querySelector('.map__pins');
 
@@ -13,16 +15,32 @@ var pinsContainer = map.querySelector('.map__pins');
 // MAIN PIN //
 //
 
+var onError = function (message) {
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var errorElement = errorTemplate.cloneNode(true);
+
+  errorElement.querySelector('.error__message').textContent = message;
+  errorElement.querySelector('.error__button').addEventListener('click', function () {
+    errorElement.remove();
+    getOffers(DATA_URL, onSuccess, onError);
+  });
+
+  mainContainer.appendChild(errorElement);
+};
+
+var onSuccess = function (data) {
+  renderPins(data.slice(0, MAX_OFFERS), pinsContainer);
+};
+
 // Activating page
 function onMainPinClick() {
   // Removing disabled state
   map.classList.remove('map--faded');
 
   // Creating offers
-  var offers = createOffers(MAX_OFFERS);
+  getOffers(DATA_URL, onSuccess, onError);
 
   // Rendering pins
-  renderPins(offers, pinsContainer);
 
   mainPin.removeEventListener('mousedown', onMainPinClick);
 }
